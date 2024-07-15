c7896ed5238ca46ffce24d32ed72b3abd59d5e9e
Victor Arvidsson <victarv@gmail.com>
2024-07-15 19:29:28 +0200

    Send email to all CFOs

--- receipt_helper/util.py
+++ receipt_helper/util.py
@@ -1,15 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from typing import Sequence
 
 from flask import current_app
 
 
-def send_email(recipient, subject, body):
+def send_email(recipients: str | Sequence[str], subject: str, body: str):
     msg = EmailMessage()
     msg.set_content(body)
     msg["Subject"] = subject
     msg["From"] = current_app.config["RECEIPTS_EMAIL_SENDER"]
-    msg["To"] = recipient
 
     try:
         with smtplib.SMTP_SSL(current_app.config["RECEIPTS_SMTP_HOST"]) as s:
@@ -17,7 +17,7 @@ def send_email(recipient, subject, body):
                 current_app.config["RECEIPTS_SMTP_USERNAME"],
                 current_app.config["RECEIPTS_SMTP_PASSWORD"],
             )
-            s.send_message(msg)
+            s.send_message(msg, to_addrs=recipients)
     except smtplib.SMTPException as ex:
         current_app.logger.error(f"Failed to send email: {type(ex).__name__}: {ex}")
         raise
