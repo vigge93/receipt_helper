3dec5ec5820fea6e223479e08a8123e2e9f98909
Victor Arvidsson <victarv@gmail.com>
2024-07-16 09:41:29 +0200

    Added ability to update usert

--- receipt_helper/forms/user_forms.py
+++ receipt_helper/forms/user_forms.py
@@ -20,6 +20,23 @@ class AddSingleUserForm(FlaskForm):
         ],
     )
 
+class UpdateUserForm(FlaskForm):
+    name = StringField(
+        "Namn",
+        [
+            validators.DataRequired("Namn krävs"),
+            validators.Length(max=100),
+        ],
+    )
+    email = StringField(
+        "Email",
+        [
+            validators.DataRequired("Email krävs"),
+            validators.Email("Måste vara en giltig Email"),
+            validators.Length(max=100),
+        ],
+    )
+
 
 class AddManyUsersForm(FlaskForm):
     file = FileField(
