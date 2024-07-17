716a9a608b4aa4407f4f861e9986a748c3498caf
Victor Arvidsson <victarv@gmail.com>
2024-07-17 22:49:01 +0200

    Use SMTP TLS instead of SSL, and added SMTP_PORT config variable

--- receipt_helper/util.py
+++ receipt_helper/util.py
@@ -12,7 +12,8 @@ def send_email(recipients: str | Sequence[str], subject: str, body: str):
     msg["From"] = current_app.config["RECEIPTS_EMAIL_SENDER"]
 
     try:
-        with smtplib.SMTP_SSL(current_app.config["RECEIPTS_SMTP_HOST"]) as s:
+        with smtplib.SMTP(current_app.config["RECEIPTS_SMTP_HOST"], port=current_app.config["RECEIPTS_SMTP_PORT"]) as s:
+            s.starttls()
             s.login(
                 current_app.config["RECEIPTS_SMTP_USERNAME"],
                 current_app.config["RECEIPTS_SMTP_PASSWORD"],
